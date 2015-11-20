604dfccca85d7433ad9247ce43217fdd725b73ca
Maxim Stoupnikov <taomax8@gmail.com>
2015-11-20 10:33:51 -0500

    Added icons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,8 @@ class HomeController < ApplicationController
 	def new
 		@results=HTTParty.get('http://ip-api.com/json')
 	    # @location="lat="+@results["lat"].to_s+"&lon="+@results["lon"].to_s
-	    @location=@results["zip"]
+	    # @location=@results["zip"]
+	    @location="Russia/Vladivostok"
 	    # @weather_url = "http://api.openweathermap.org/data/2.5/weather?q=#{@location}&APPID=6f188d89b8bd85b5e4911df9b542941c"
 	    
 	    @weather_url = "http://api.wunderground.com/api/ed4b5bf53486878a/geolookup/conditions/q/#{@location}.json"
