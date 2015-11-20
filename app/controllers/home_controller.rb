148add6c2f00d79ba9ebaba18e3cb9aa53f68879
Maxim Stoupnikov <taomax8@gmail.com>
2015-11-20 13:17:05 -0500

    Search function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,28 @@ class HomeController < ApplicationController
 	end
 
 	def new
-		@results=HTTParty.get('http://ip-api.com/json')
+		puts params[:location]
+		if params[:location].nil?
+			@results=HTTParty.get('http://ip-api.com/json')
 	    # @location="lat="+@results["lat"].to_s+"&lon="+@results["lon"].to_s
-	    @location=@results["zip"]
+	    	@location_new=@results["zip"]
 	    # @weather_url = "http://api.openweathermap.org/data/2.5/weather?q=#{@location}&APPID=6f188d89b8bd85b5e4911df9b542941c"
-	    
-	    @weather_url = "http://api.wunderground.com/api/ed4b5bf53486878a/geolookup/conditions/q/#{@location}.json"
+	    else 
+	    	@location=Location.new location_params
+	    	# @location.city=@location.city.gsub! ' ', ''
+	    	# @location.state=@location.state.gsub! ' ', ''
+	    	@location_new=@location.state+"/"+@location.city
+	    end
+	    @weather_url = "http://api.wunderground.com/api/ed4b5bf53486878a/geolookup/conditions/q/#{@location_new}.json"
     	@weather = HTTParty.get(@weather_url)
-    	@time = Time.new.strftime(" %A %B-%d-%Y %H:%M:%S:%p")
+    	@new_location=Location.new
     	
+    end
+
+    private
+
+    def location_params
+     	params.require(:location).permit(:city, :state)
     end
 end
 # ed4b5bf53486878a
